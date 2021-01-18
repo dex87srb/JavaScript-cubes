40fcb9cc90f68ed33068e2d2145828e20e5b978d
Danijel Tomic <dex87srb@gmail.com>
2021-01-18 03:21:30 +0100

    JS-Functions solved tasks

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/*JavaScript variables and operator*/
+/*JavaScript variables and operators*/
 
 var task3 = document.getElementById("task3");
 
@@ -127,9 +127,9 @@ function CheckUserAge() {
 
 /*JS ARRAYS & LOOPS*/
 
-/*task 1*/
-
 console.log("JS ARRAYS & LOOPS");
+
+/*task 1*/
 
 var dataOld = [34, true, "Peter", 1992];
 var dataNew = [];
@@ -240,3 +240,171 @@ for (var i = 0; i < numA; i++) {
   alt += str + "\n";
 }
 console.log(alt);
+console.log("\n");
+
+/*JS FUNCTIONS*/
+
+console.log("JS FUNCTIONS");
+
+/*task 1*/
+
+var arrayFunction = [0, 1, 2, 3, 4];
+
+var reverseArray = function (array) {
+  var newArr = [];
+  for (var i = 0; i < array.length; i++) {
+    newArr[newArr.length] = array[array.length - 1 - i];
+  }
+  return newArr;
+};
+
+console.log("TASK 1");
+console.log(reverseArray(arrayFunction));
+console.log("\n");
+
+/*task 2*/
+
+function ReceiveAnyPar(any) {
+  console.log(any);
+}
+
+console.log("TASK 2");
+ReceiveAnyPar(typeof "Dex");
+console.log("\n");
+
+/*task 3*/
+
+var arrayNames = ["Dex", "Max", "Lauren", "Nile"];
+var newArrayName = arrayNames[0];
+
+var longestName = function (array, newArray) {
+  for (var i = 0; i < array.length; i++) {
+    for (var j = 0; j < array[i].length; j++) {
+      if (newArray.length < array[i].length) newArray = array[i];
+    }
+  }
+  console.log(newArray);
+};
+
+console.log("TASK 3");
+longestName(arrayNames, newArrayName);
+console.log("\n");
+
+/*task 4*/
+
+var arrayLandH = [32, 41, 5, 6, 3, 8, 39];
+var lowest = arrayLandH[0];
+var highest = arrayLandH[0];
+
+function SecondLowestAndHighest(array, lowest, highest) {
+  var newArray = [];
+
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] < lowest) lowest = array[i];
+    if (array[i] > highest) highest = array[i];
+  }
+
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] != lowest && array[i] != highest)
+      newArray[newArray.length] = array[i];
+  }
+
+  var secondHighest = newArray[0];
+  var secondLowest = newArray[0];
+
+  for (var i = 0; i < newArray.length; i++) {
+    if (newArray[i] < secondLowest) secondLowest = newArray[i];
+    if (newArray[i] > secondHighest) secondHighest = newArray[i];
+  }
+
+  console.log(secondLowest, secondHighest);
+}
+
+console.log("TASK 4");
+SecondLowestAndHighest(arrayLandH, lowest, highest);
+console.log("\n");
+
+/*task 5*/
+
+var arrayOfNumbers = [4, 65, 75, 78];
+var providedNumber = 77;
+
+function ProvideBiggerNumberArray(array, number) {
+  CheckNumbersBigger(array, number);
+}
+
+function CheckNumbersBigger(array, number) {
+  var newArray = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] > number) newArray[newArray.length] = array[i];
+  }
+  console.log(newArray);
+}
+
+console.log("TASK 5");
+ProvideBiggerNumberArray(arrayOfNumbers, providedNumber);
+console.log("\n");
+
+/*task 6*/
+
+var arrayMaximum = [15, -2, 22, 0, 13];
+var maximumAndLowNumber = arrayMaximum[0];
+
+function MaxNum(array, maxNumber) {
+  var maxNumber;
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] > maxNumber) maxNumber = array[i];
+  }
+
+  return maxNumber;
+}
+
+function LowNum(array, lowNumber) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] < lowNumber) lowNumber = array[i];
+  }
+
+  return lowNumber;
+}
+
+function MultiLowAndHigh(array, maxAndLow) {
+  var max = MaxNum(array, maxAndLow);
+  var low = LowNum(array, maxAndLow);
+
+  console.log(max * low);
+}
+
+console.log("TASK 6");
+MultiLowAndHigh(arrayMaximum, maximumAndLowNumber);
+console.log("\n");
+
+/*task 7*/
+
+var arrayBig = [15, 35, 46, 23, 15, 17, 23, 24, 35, 12, 72, 64, 35, 22, 64];
+
+function UniqueWithoutBiggestN(array) {
+  var unique = array.filter((item, i, ar) => ar.indexOf(item) === i);
+  //morao da se sluzim ovom metodom, jer se nisam snasao manuelno...
+
+  BiggestDelete(unique);
+}
+
+var BiggestDelete = function (array) {
+  var biggestNum = array[0];
+  var newArray = [];
+
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] > biggestNum) biggestNum = array[i];
+  }
+
+  for (var j = 0; j < array.length; j++) {
+    if (array[j] !== biggestNum) {
+      newArray[newArray.length] = array[j];
+    }
+  }
+
+  console.log(newArray);
+};
+
+console.log("TASK 7");
+UniqueWithoutBiggestN(arrayBig, BiggestDelete);
