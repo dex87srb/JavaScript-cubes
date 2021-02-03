17bb6367c4a7a56588ca2c307f2c0f8a40f8d688
Danijel Tomic <dex87srb@gmail.com>
2021-02-03 02:03:16 +0100

    JS-Functions,Arrays&Objects classroom practice - solved tasks 1,2,3,4,5 and task 2 from JS-Functions,Arrays&Objects homework corrected

--- scriptOOP.js
+++ scriptOOP.js
@@ -133,9 +133,9 @@ console.log("TASK 7");
 console.log(person);
 console.log("\n");
 
-/*JS ARRAYS, FUNCTIONS & OBJECTS*/
+/*JS ARRAYS, FUNCTIONS & OBJECTS* homework*/
 
-console.log("JS ARRAYS, FUNCTIONS & OBJECTS");
+console.log("JS ARRAYS, FUNCTIONS & OBJECTS homework");
 
 /*task 1*/
 
@@ -161,19 +161,152 @@ console.log("\n");
 
 console.log("TASK 2");
 
-function logNameAndAge(anyObject) {
-  console.log(anyObject.name + " " + anyObject.age);
+class LogNameAndAge {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+    console.log(this.name + " " + this.age);
+  }
 }
 
-var arrayObjects = [
-  {
-    name: "Danijel",
-    age: 33,
-  },
-  { name: "Milan", age: 23 },
-  { name: "Marija", age: 35 },
-];
+var array = [];
 
-for (var i = 0; i < arrayObjects.length; i++) {
-  logNameAndAge(arrayObjects[i]);
+array[0] = new LogNameAndAge("Danijel", 23);
+array[1] = new LogNameAndAge("Milos", 20);
+
+console.log("\n");
+
+/*JS ARRAYS, FUNCTIONS & OBJECTS* classroom practice*/
+
+console.log("JS ARRAYS, FUNCTIONS & OBJECTS classroom practice");
+
+/*task 1*/
+
+var someDataObject = {
+  name: "Peter",
+  lastName: "Dinklage",
+  status: "married",
+};
+
+class MakeReplicaObject {
+  constructor(data) {
+    this.name = data.name;
+    this.lastName = data.lastName;
+    this.status = data.status;
+  }
 }
+
+var newObj = new MakeReplicaObject(someDataObject);
+
+console.log("TASK 1");
+console.log(newObj);
+console.log("\n");
+
+/*task 2*/
+
+var someDataArray = [13, 45, 56, [32, 11], 27, [55, 92]];
+
+var repackArrayInOne = function (array) {
+  var newA = [];
+  for (var i = 0; i < array.length; i++) {
+    if (array[i].length > 0) {
+      for (var j = 0; j < array[i].length; j++) {
+        newA[newA.length] = array[i][j];
+      }
+    } else newA[newA.length] = array[i];
+  }
+
+  console.log(newA);
+};
+
+console.log("TASK 2");
+repackArrayInOne(someDataArray);
+console.log("\n");
+
+/*task 3*/
+
+var newA = [];
+var newB = [];
+var joinedArrays = [];
+var splitFunction;
+
+var splitInTwoArray = function (array) {
+  for (var i = 0; i < array.length; i++) {
+    if (
+      array[i].length > 0 &&
+      array.indexOf(array[i]) < array.indexOf(array[i + 1]) //u c# bi bilo "out of boundaries exception", jer zadnja iteracija na [i+1] je veca od zadnjeg indeksa, vidim ovde radi! :)
+    ) {
+      for (var j = 0; j < array[i].length; j++) {
+        newA[newA.length] = array[i][j];
+      }
+    } else
+      for (var j = 0; j < array[i].length; j++) {
+        newB[newB.length] = array[i][j];
+      }
+  }
+};
+
+var joinTwoArray = function (split) {
+  return (splitFunction = function (data) {
+    split(data);
+
+    for (var i = 0; i < newA.length; i++)
+      joinedArrays[joinedArrays.length] = newA[i];
+
+    for (var i = 0; i < newB.length; i++) {
+      joinedArrays[joinedArrays.length] = newB[i];
+    }
+    console.log(joinedArrays);
+  });
+};
+
+console.log("TASK 3");
+joinTwoArray(splitInTwoArray)(someDataArray);
+console.log("\n");
+
+/*task 4*/
+
+function checkName(data) {
+  //ovde nisam siguran jel se trazi da se obrise property ili value, nije definisano?!
+  //should remove name from the function - jel ovo typo greska ili?
+  if (data.name) {
+    return function () {
+      delete data.name;
+    };
+  } else
+    return function (name) {
+      data.name = name;
+    };
+}
+
+checkName(someDataObject)("Peter");
+
+console.log("TASK 4");
+console.log(someDataObject);
+console.log("\n");
+
+/*task 5*/
+
+var someData = {
+  name: "Peter",
+  lastName: "Dinklage",
+  status: "married",
+};
+
+console.log("TASK 5");
+//should remove name from the function - jel ovo typo greska ili?
+someDataObject.checkNameAndSet = function (data) {
+  if (data.name) {
+    data.method = (function () {
+      delete data.name;
+      console.log(data);
+
+      (function (name) {
+        data.name = name;
+        console.log(data);
+      })("Peter");
+    })();
+  }
+};
+
+someDataObject.checkNameAndSet(someData);
